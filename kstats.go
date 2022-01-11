8447c4baa1b3a4ce3f13409b2683b1282e7cae02
Konrad R.K. Ludwig <konrad.rk.ludwig@gmail.com>
2022-01-11 11:30:32 -0500

    Add basic SampleVariance() function

--- kstats.go
+++ kstats.go
@@ -1,5 +1,7 @@
 package kstats
 
+import "math"
+
 // SampleMean calculates a point estimate of the mean of an array of observations.
 func SampleMean(obs []float64) float64 {
 	var sum, mean float64
@@ -8,4 +10,16 @@ func SampleMean(obs []float64) float64 {
 	}
 	mean = sum / float64(len(obs))
 	return mean
+}
+
+// SampleVariance calculates a point estimate of the variance of an array of observations.
+func SampleVariance(obs []float64) float64 {
+	var sample_mean, squared_error, sample_variance float64
+	sample_mean = SampleMean(obs)
+	for i := 0; i < len(obs); i++ {
+		diff := obs[i] - sample_mean
+		squared_error += math.Pow(diff, 2)
+	}
+	sample_variance = squared_error / float64(len(obs)-1)
+	return sample_variance
 }
