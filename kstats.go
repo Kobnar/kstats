6a1246a89127e42db7069d16551e8fb97f49854a
Konrad R.K. Ludwig <konrad.rk.ludwig@gmail.com>
2022-01-11 11:57:33 -0500

    Rename variables for mathematical clarity and brevity

--- kstats.go
+++ kstats.go
@@ -2,51 +2,52 @@ package kstats
 
 import "math"
 
-// SampleMean calculates a point estimate of the mean of an array of observations.
-func SampleMean(obs []float64) float64 {
-	var sum, mean float64
-	for i := 0; i < len(obs); i++ {
-		sum += obs[i]
+// Mean calculates a point estimate of the mean of an array of observations.
+func Mean(x []float64) float64 {
+	var sm, mn float64
+	for i := 0; i < len(x); i++ {
+		sm += x[i]
 	}
-	mean = sum / float64(len(obs))
-	return mean
+	mn = sm / float64(len(x))
+	return mn
 }
 
 // WeightedMean calculates a weighted mean of an array of observations given an array of weights.
-func WeightedMean(obs []float64, wgts []float64) float64 {
-	var obs_sum, wgts_sum, mean float64
-	for i := 0; i < len(obs); i++ {
-		obs_sum += obs[i] * wgts[i]
-		wgts_sum += wgts[i]
+func WeightedMean(x []float64, w []float64) float64 {
+	var xw_sm, w_sm, wmn float64
+	for i := 0; i < len(x); i++ {
+		xw_sm += x[i] * w[i]
+		w_sm += w[i]
 	}
-	mean = obs_sum / wgts_sum
-	return mean
+	wmn = xw_sm / w_sm
+	return wmn
 }
 
 // GeometricMean calculates a geometric mean of an array of observations (used in geometic growth contexts).
-func GeometricMean(obs []float64) float64 {
-	var prod, mean float64
-	prod = 1.0
-	for i := 0; i < len(obs); i++ {
-		prod *= obs[i]
+func GeometricMean(x []float64) float64 {
+	var n, prd, gmn float64
+	n = float64(len(x))
+	prd = 1.0
+	for i := 0; i < len(x); i++ {
+		prd *= x[i]
 	}
-	mean = math.Pow(prod, 1.0/float64(len(obs)))
-	return mean
+	gmn = math.Pow(prd, 1.0/n)
+	return gmn
 }
 
 // SampleVariance calculates a point estimate of the variance of an array of observations.
-func SampleVariance(obs []float64) float64 {
-	var sample_mean, squared_error, sample_variance float64
-	sample_mean = SampleMean(obs)
-	for i := 0; i < len(obs); i++ {
-		diff := obs[i] - sample_mean
-		squared_error += math.Pow(diff, 2)
+func SampleVariance(x []float64) float64 {
+	var mn, se, vr float64
+	mn = Mean(x)
+	for i := 0; i < len(x); i++ {
+		diff := x[i] - mn
+		se += math.Pow(diff, 2)
 	}
-	sample_variance = squared_error / float64(len(obs)-1)
-	return sample_variance
+	vr = se / float64(len(x)-1)
+	return vr
 }
 
 // ZScore calculates the z-score for a particular observation.
-func ZScore(obs float64, mean float64, sd float64) float64 {
-	return (obs - mean) / sd
+func ZScore(x float64, mn float64, sd float64) float64 {
+	return (x - mn) / sd
 }
